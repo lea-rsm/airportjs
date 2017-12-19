51b0df0320f0f2c5da1a7fd908af288617cd554e
newtdogg <edwardfrancisnewton@live.com>
2017-12-19 15:25:22 +0000

    added jasminespy for plane

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,7 +1,10 @@
 describe("Airport", function() {
 
   var airport;
-  var plane;
+  var plane = jasmine.createSpyObj('plane1', ['land']);
+  plane.land.and.callFake(function() {
+    return true;
+  })
 
   beforeEach(function() {
     airport = new Airport();
@@ -12,19 +15,23 @@ describe("Airport", function() {
     expect(airport.landedPlanes.length).toEqual(0);
   });
 
-  it("can land a plane in the airport", function() {
-    airport.land(plane)
-    expect(airport.landedPlanes.length).toEqual(1);
+  describe("Landing", function() {
+    it("can land a plane in the airport", function() {
+      airport.land(plane)
+      expect(airport.landedPlanes.length).toEqual(1);
+    });
   });
 
-  it("can instruct a plane to take off", function(){
-    airport.land(plane)
-    airport.takeoff()
-    expect(airport.landedPlanes.length).toEqual(0);
-  });
+  describe("Take Off", function() {
+    it("can instruct a plane to take off", function(){
+      airport.land(plane)
+      airport.takeoff()
+      expect(airport.landedPlanes.length).toEqual(0);
+    });
 
-  it("confirms that it is no longer in the airport", function(){
-    airport.land(plane)
-    expect(airport.takeoff()).toEqual("the plane took off");
+    it("confirms that it is no longer in the airport", function(){
+      airport.land(plane)
+      expect(airport.takeoff()).toEqual("the plane took off");
+    });
   });
 });
